8ea89b23696c0abc1eda80dc99f9674bc670a0ca
Jom Jose <jomjose@users.noreply.github.com>
2014-04-22 12:10:27 +0530

    Improved bubble sort  with new array

--- bubblesort/bubblesort.js
+++ bubblesort/bubblesort.js
@@ -26,3 +26,39 @@ function bubbleSort(a) {
     bubbleSort(a);
   }
 }
+
+
+
+/**
+* Improved bubble sort  with new array
+*
+* 
+* 
+* 
+*/
+
+function bubbleSort(a, b) {
+  var swapped = false,
+    i = 1,
+    j = a.length,
+    tmp;
+ 
+  if (! b) {
+    b = 0;
+  }
+ 
+  j -= b;
+ 
+  for (; i < j; i += 1) {
+    if (a[i - 1] > a[i]) {
+      tmp = a[i];
+      a[i] = a[i - 1];
+      a[i - 1] = tmp;
+      swapped = true;
+    }
+  }
+ 
+  if (swapped) {
+    bubbleSort(a, b + 1);
+  }
+}
