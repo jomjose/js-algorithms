ebcc2b4eec9fa25d213b6244cdf60ec25facfa7d
Jom Jose <jomjose@users.noreply.github.com>
2014-04-23 23:20:48 +0530

    Update heapsort.js : Added use strict

--- heapsort/heapsort.js
+++ heapsort/heapsort.js
@@ -41,3 +41,45 @@
         heapify(array, largest, heapSize, cmp);
       }
     }
+    
+
+    /**
+     * Builds max heap from given array.
+     *
+     * @private
+     * @param {array} array Array which should be turned into max heap
+     * @returns {array} array Array turned into max heap
+     */
+    function buildMaxHeap(array, cmp) {
+      for (var i = Math.floor(array.length / 2); i >= 0; i -= 1) {
+        heapify(array, i, array.length, cmp);
+      }
+      return array;
+    }
+
+    /**
+     * Heapsort. Turns the input array into max heap and after that sorts it.
+     *
+     * @public
+     * @param {array} array Input array
+     * @returns {array} array Sorted array
+     */
+    return function (array, cmp) {
+      cmp = cmp || comparator;
+      var size = array.length,
+          temp;
+      buildMaxHeap(array, cmp);
+      for (var i = array.length - 1; i > 0; i -= 1) {
+        temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        size -= 1;
+        heapify(array, 0, size, cmp);
+      }
+      return array;
+    };
+  }());
+
+  exports.heapSort = heapSort;
+
+}(typeof exports === 'undefined' ? window : exports));
